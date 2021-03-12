3fcac4aab6a26f3ecde3bd8ef50bc1fcebe5b07d
binhcn <binhcaong@gmail.com>
2021-03-12 17:46:44 +0700

    finish fresh scope

--- src/main/java/dev/binhcn/Application.java
+++ src/main/java/dev/binhcn/Application.java
@@ -1,15 +1,34 @@
 package dev.binhcn;
 
+import dev.binhcn.config.TpeConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
+@RefreshScope // if @Value, please must use, else @ConfigurationProperties doesn't need to attach
+@RestController
+@RequiredArgsConstructor
 public class Application {
+
+    @Value("${welcome.message}")
+    String welcomeText;
+
+    private final TpeConfig tpeConfig;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String welcomeText() {
+        return welcomeText + tpeConfig.toString();
     }
 
 }
--- src/main/java/dev/binhcn/config/TpeConfig.java
+++ src/main/java/dev/binhcn/config/TpeConfig.java
@@ -2,7 +2,6 @@ package dev.binhcn.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
@@ -10,7 +9,6 @@ import java.util.List;
 @Configuration
 @ConfigurationProperties(prefix = "tpe-config")
 @Data
-//@RefreshScope
 public class TpeConfig extends BaseExternalConfig {
     private List<String> redisNodes;
 }
