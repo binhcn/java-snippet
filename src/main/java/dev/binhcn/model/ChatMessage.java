1941ad0d804b0a8a8e10c47cd34dc8f524133368
binhcn <binhcaong@gmail.com>
2021-03-15 01:08:47 +0700

    finish project

---
+++ src/main/java/dev/binhcn/model/ChatMessage.java
@@ -0,0 +1,16 @@
+package dev.binhcn.model;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
