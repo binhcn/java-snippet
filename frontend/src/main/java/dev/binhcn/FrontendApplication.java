6001de39518058959ea0539dc9bec2d1e408e9cf
binhcn <binhcaong@gmail.com>
2021-03-16 16:49:49 +0700

    add k8s

---
+++ frontend/src/main/java/dev/binhcn/FrontendApplication.java
@@ -0,0 +1,30 @@
+package dev.binhcn;
+
+import java.net.UnknownHostException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class FrontendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FrontendApplication.class, args);
+  }
+
+  @GetMapping
+  public String helloWorld() throws UnknownHostException {
+
+    RestTemplate restTemplate = new RestTemplate();
+//    String resourceUrl = "http://demo-backend:8080";
+    String resourceUrl = "http://localhost:8080";
+    ResponseEntity<String> response
+        = restTemplate.getForEntity(resourceUrl, String.class);
+    return "Message from backend is: " + response.getBody();
+  }
+
+}
--- src/main/java/dev/binhcn/Application.java
+++ src/main/java/dev/binhcn/Application.java
@@ -1,13 +1,24 @@
 package dev.binhcn;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("")
+    public String helloWorld() throws UnknownHostException {
+
+        return "Hello from " + InetAddress.getLocalHost().getHostName();
     }
 
 }
