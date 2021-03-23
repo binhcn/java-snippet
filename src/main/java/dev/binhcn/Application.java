70de308c4453b0b0615733b3c0f100e52cb329dd
binhcaong <binhcn@vng.com.vn>
2021-03-23 10:37:26 +0700

    add

--- src/main/java/dev/binhcn/Application.java
+++ src/main/java/dev/binhcn/Application.java
@@ -1,11 +1,11 @@
 package dev.binhcn;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* https://roytuts.com/spring-conditionalonproperty-example/ */
 @SpringBootApplication
 @RequiredArgsConstructor
 public class Application implements CommandLineRunner {
