05860220cf10703b0f31cf2187a5a1249b2166e9
binhcaong <binhcn@vng.com.vn>
2021-03-24 18:00:27 +0700

    add

--- src/main/java/dev/binhcn/Application.java
+++ src/main/java/dev/binhcn/Application.java
@@ -1,22 +1,14 @@
 package dev.binhcn;
 
-import dev.binhcn.model.RedisConfig;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
 
 /** https://github.com/markramach/spring-boot-starter-consul */
 @SpringBootApplication
-@RequiredArgsConstructor
 public class Application {
-
-    private final RedisConfig redisConfig;
-    private final Environment env;
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class, args);
-
     }
 }
