a3aab64eec983d7bbfbb5a1900582db3fac1d088
binhcaong <binhcn@vng.com.vn>
2021-03-24 07:21:20 +0700

    add file

---
+++ src/main/java/dev/binhcn/concurrent/ConcurrentQueue.java
@@ -0,0 +1,73 @@
+package dev.binhcn.concurrent;
+
+import java.util.ArrayDeque;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConcurrentQueue<T> {
+
+    private int capacity;
+    private AtomicInteger size = new AtomicInteger(0);
+    private ArrayDeque<T> arrayDeque;
+
+    private ReentrantLock readLock = new ReentrantLock();
+    private AtomicBoolean shutdown = new AtomicBoolean(false);
+
+    protected CountDownLatch shutdownCountDownLatch = new CountDownLatch(1);
+
+    public ConcurrentQueue(int capacity) {
+        this.capacity = capacity;
+        this.arrayDeque = new ArrayDeque<T>(capacity + 10);
+    }
+
+    public T take() {
+        readLock.lock();
+        try {
+            while (true) {
+                if (size.get() == 0) {
+                    if (shutdown.get()) {
+                        return null;
+                    } else {
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } else {
+                    size.decrementAndGet();
+                    return arrayDeque.removeFirst();
+                }
+            }
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public void put(T item) {
+        while (true) {
+            if (size.get() >= capacity) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                addItem(item);
+                return;
+            }
+        }
+    }
+
+
+    protected void addItem(T item) {
+        arrayDeque.addLast(item);
+        size.incrementAndGet();
+    }
+
+    public void onConsumerFinished() {
+        shutdownCountDownLatch.countDown();
+    }
+}
---
+++ src/main/java/dev/binhcn/concurrent/FastBlockingExecutor.java
@@ -0,0 +1,19 @@
+package dev.binhcn.concurrent;
+
+import java.util.concurrent.Executor;
+
+
+public class FastBlockingExecutor implements Executor {
+
+	private ConcurrentQueue<Runnable> concurrentQueue;
+	
+	public FastBlockingExecutor(String name, int queueSize, int threadCount) {
+		concurrentQueue = new ConcurrentQueue<Runnable>(queueSize);
+		new ConcurrentQueueConsumerThreadPool(name, concurrentQueue, threadCount);
+	}
+	
+	@Override
+	public void execute(Runnable command) {
+		concurrentQueue.put(command);
+	}
+}
