e70d326e5a5141ebca2cb160f40dfb22b96e865f
binhcaong <binhcn@vng.com.vn>
2021-07-19 22:57:49 +0700

    update code

--- src/main/java/dev/binhcn/Application.java
+++ src/main/java/dev/binhcn/Application.java
@@ -1,8 +1,46 @@
 package dev.binhcn;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import vn.zalopay.migrator.rollout.client.RolloutClient;
+import vn.zalopay.migrator.rollout.client.RolloutClientImpl;
+import vn.zalopay.migrator.rollout.client.entity.RolloutConfig;
+import vn.zalopay.migrator.rollout.client.exception.RolloutException;
+import vn.zalopay.migrator.rollout.client.grpc.RolloutClientStub;
+
+@Slf4j
 public class Application {
 
-    public static void main(String[] args) {
+    private static final Logger LOGGER = LogManager.getLogger(Application.class.getName());
 
+    public static void main(String[] args) {
+//        LOGGER.debug("Debug Message Logged !!!");
+//        LOGGER.info("Info Message Logged !!!");
+//        LOGGER.error("Error Message Logged !!!", new Exception("NullError"));
+
+//        log.debug("Debug Message Logged !!!");
+//        log.info("Info Message Logged !!!");
+//        log.error("Error Message Logged !!!", new Exception("NullError"));
+
+        RolloutConfig rolloutConfig = new RolloutConfig();
+        rolloutConfig.setGrpcHost("localhost");
+        rolloutConfig.setGrpcPort(8544);
+        rolloutConfig.setPoolSize(1);
+        rolloutConfig.setTimeoutInSeconds(1);
+        rolloutConfig.setUseSsl(false);
+
+        RolloutClientStub rolloutClientStub = new RolloutClientStub(rolloutConfig);
+        RolloutClient client = RolloutClientImpl.builder().rolloutClientStub(rolloutClientStub).build();
+
+        long userId = 1086547658145792L;
+
+        boolean isRolledOut = false;
+        try {
+            isRolledOut = client.isRolledOut(userId);
+        } catch (RolloutException e) {
+            e.printStackTrace();
+        }
+        System.out.println(isRolledOut);
     }
 }
---
+++ src/main/java/dev/binhcn/Student.java
@@ -0,0 +1,10 @@
+package dev.binhcn;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Student {
+  private final int age;
+}
